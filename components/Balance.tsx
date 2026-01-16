544e27b405a57f1ff68faab26fd3c890b928b3d6
sepowelltm-ops <sepowelltm@gmail.com>
2026-01-15 18:45:34 -0600

    Update Balance.tsx

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Balance() {
   const [bal, setBal] = useState(0);
